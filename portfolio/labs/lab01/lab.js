e52604d3f8666af20a400821d3fd30c122145007
Eric891113 <52520244+Eric891113@users.noreply.github.com>
2019-07-08 14:25:19 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,7 +4,38 @@
 
 
 function start(e) {
+var name=[
+{
+    char:'楊',
+    big5:'87A8',
+    cns:'1-644E',
+    unicode:'694A'
+},
+{
+  char:'宗',
+    big5:'A976',
+    cns:'1-4C78',
+    unicode:'5B97'
+},
+{
+  char:'諭',
+    big5:'BFD9',
+    cns:'1-7243',
+    unicode:'8AED'
+}
+];
+var ele=document.getElementById("div1");
 
+for(var i=0;i<3; i++){
+ ele.innerHTML  += name[i].char;
+ ele.innerHTML  += 'big5'
+ ele.innerHTML  += name[i].big5;
+ ele.innerHTML  += 'cns';
+ ele.innerHTML  += name[i].cns;
+ ele.innerHTML  += name[i].unicode;
+ ele.innerHTML  += '<br>';
+
+};
 }
 
 /*
@@ -16,3 +47,4 @@ true - The event handler is executed in the capturing phase
 false- Default. The event handler is executed in the bubbling phase
 */
 window.addEventListener( "load", start, false );
+
